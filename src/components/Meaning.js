5d0a95a1adceb264b8c2c3e679f0f8eaf6358f1f
Beachyshells <151598119+Beachyshells@users.noreply.github.com>
2025-10-22 14:57:08 -0400

    added the conditional logic and provided the html with the props used for display

--- src/components/Meaning.js
+++ src/components/Meaning.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-export default function Meaning() {
-  return <div className="Meaning">These are my meanings</div>;
+export default function Meaning(props) {
+
+  if (props.meanings) {
+props.meanings.map(function()){
+  
+}
+    return (
+      <div className="Meaning">
+        <h3>{props.meaning.partOfSpeech}</h3>
+        <Synonyms meaning={props.meanings} />
+      </div>
+    );
+  
 }
